907071418a3250fcff3e6fe7d8a82534ea46ad88
owen <owen@cloudgiant.ca>
2018-12-06 10:47:49 -0500

    Part 1

---
+++ api/routes/users.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+// Include express router middleware
+const router = express.Router();
+
+// Add a 'get' method to express router for our test route
+router.get("/", function(req, res) {
+  res.send({ msg: "Hello World" });
+});
+
+// Exports the router object
+module.exports = router;
